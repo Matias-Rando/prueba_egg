397e76d742b2a7490298e2566a72d2589ac4e51d
Matias Rando <matrando33@gmail.com>
2021-10-14 20:49:38 -0300

    Commit inicial

---
+++ src/proyecto/entidades/Auto.java
@@ -0,0 +1,33 @@
+package proyecto.entidades;
+
+public class Auto {
+    
+    private String modelo;
+    private String marca;
+    private int anio;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+}
